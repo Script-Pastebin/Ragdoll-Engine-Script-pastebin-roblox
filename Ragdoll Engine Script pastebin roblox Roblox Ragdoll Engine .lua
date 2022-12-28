edb50c829ffe6d97ca82ad08f91d45b4317db9cb
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:52:21 +0100

    commit zero

---
+++ Ragdoll Engine Script pastebin roblox Roblox Ragdoll Engine .lua
@@ -0,0 +1,42 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+a=game.Players:FindFirstChild("OddPotion",true)
+local Players = game:GetService("Players")
+local localPlayer = Players.LocalPlayer
+local backpack = localPlayer:WaitForChild("Backpack")
+ 
+local tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Parent = backpack
+tool.Name = "Invisible Object"
+tool.Equipped:Connect(function(mouse)
+mouse.Button1Down:Connect(function()
+if mouse.Target and mouse.Target.Parent then
+a.TransEvent:FireServer(mouse.Target,1)
+end
+end)
+end)
+a=game.Players:FindFirstChild("OddPotion",true)
+local Players = game:GetService("Players")
+local localPlayer = Players.LocalPlayer
+local backpack = localPlayer:WaitForChild("Backpack")
+ 
+local tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Parent = backpack
+tool.Name = "Visible Object"
+tool.Equipped:Connect(function(mouse)
+mouse.Button1Down:Connect(function()
+if mouse.Target and mouse.Target.Parent then
+a.TransEvent:FireServer(mouse.Target,0)
+end
+end)
+end)
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Roblox Ragdoll Engine Invisibility Tools!
+						
+]]
